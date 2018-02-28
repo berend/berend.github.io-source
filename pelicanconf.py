a69c0a943f21e015c29ef445e4f74a9f8bc792da
Berend Kapelle <berend.kapelle@gmail.com>
2018-02-28 22:20:51 +0100

    serve more files as static

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,7 +33,6 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-
 THEME = './theme/'
 
 ABOUT_PAGE = '/pages/about.html'
@@ -45,7 +44,15 @@ AUTHOR_BLOG = 'http://berend.github.io'
 RELATIVE_URLS = True
 ENABLE_MATHJAX = True
 
-STATIC_PATHS = ['images', 'resources', 'favicon.ico']
+STATIC_PATHS = ['images', 'resources', 'favicon.ico', 'android-icon-144x144.png',
+                'android-icon-192x192.png', 'android-icon-36x36.png', 'android-icon-48x48.png',
+                'android-icon-72x72.png', 'android-icon-96x96.png', 'apple-icon-114x114.png',
+                'apple-icon-120x120.png', 'apple-icon-144x144.png', 'apple-icon-152x152.png',
+                'apple-icon-180x180.png', 'apple-icon-57x57.png', 'apple-icon-60x60.png',
+                'apple-icon-72x72.png', 'apple-icon-76x76.png', 'apple-icon-precomposed.png',
+                'apple-icon.png', 'browserconfig.xml', 'favicon-16x16.png', 'favicon-32x32.png',
+                'favicon-96x96.png', 'manifest.json', 'ms-icon-144x144.png',
+                'ms-icon-150x150.png', 'ms-icon-310x310.png', 'ms-icon-70x70.png']
 
 MARKDOWN = {
     'extension_configs': {
